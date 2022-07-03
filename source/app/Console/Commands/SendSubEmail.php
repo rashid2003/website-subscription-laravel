7f5f6adb4a9552cefdaebdc2b191813e2a6e1844
Rashid Obaidi <31435017+rashid2003@users.noreply.github.com>
2022-07-03 14:22:12 +0000

    fix send email

--- source/app/Console/Commands/SendSubEmail.php
+++ source/app/Console/Commands/SendSubEmail.php
@@ -33,13 +33,15 @@ class SendSubEmail extends Command
         $post = $this->argument('post');
 
         $user = \App\Models\Subscriber::where('website_id', $post['$post']['website_id'])->first();
+        Log::info('good');
 
         $data = ['name'=> "Sender Name", "body" => $post['$post']['description']];
 
-        Mail::send(['text'=>'mail'], $data, function($message) {
-            $message->to($user->email, $user->name)->subject
-                ($post['$post']['title']);
-            $message->from('xyz@gmail.com','Rashid Obaidi');
+
+        Mail::raw($post['$post']['content'], function ($message) use($user, $post) {
+            $message->from('rashid@test.com', 'Rashid');
+            $message->to($user->email, $user->name);
+            $message->subject($post['$post']['title']);
         });
         $this->info('The email send successfully!');
     }
